5541ec20945e1be3242973636663735d69d920dd
蒋凯 <Joker@jiangkaideMacBook-Pro.local>
2018-07-23 21:20:48 +0800

    init

---
+++ src/coin/main.go
@@ -0,0 +1,10 @@
+package coin
+
+import (
+	"core"
+)
+
+func main() {
+	cli := core.CLI{}
+	cli.Run()
+}
---
+++ src/core/cli_createblockchain.go
@@ -0,0 +1,19 @@
+package core
+
+import (
+	"fmt"
+	"log"
+)
+
+func (cli *CLI) createBlockchain(address, nodeID string) {
+	if !ValidateAddress(address) {
+		log.Panic("ERROR: Address is not valid")
+	}
+	bc := CreateBlockchain(address, nodeID)
+	defer bc.db.Close()
+
+	UTXOSet := UTXOSet{bc}
+	UTXOSet.Reindex()
+
+	fmt.Println("Done!")
+}
